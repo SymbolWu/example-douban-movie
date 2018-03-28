123c466520f769543fa0618dce6a4c2e46c80093
Wu Xinbo <bluessoul@sina.com>
2018-03-29 00:49:03 +0800

    添加正在热映

--- src/components/Movie/InTheaters.js
+++ src/components/Movie/InTheaters.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 
-import { Row, Col } from 'antd';
+import { Row, Col,Card } from 'antd';
 import './css/intheaters.css'
 class InTheaters extends Component{
   // constructor(props) {
@@ -8,25 +8,33 @@ class InTheaters extends Component{
   // }
 
   render(){
-
+    let {intheaters_arr} = this.props;
+    console.log(intheaters_arr);
     return(
       <div className="gutter-example">
         <h3>正在热映</h3>
           <Row gutter={16}>
-            <Col className="gutter-row" span={6}>
-              <div className="gutter-box">.ant-col-6</div>
-            </Col>
-            <Col className="gutter-row" span={6}>
-              <div className="gutter-box">.ant-col-6</div>
-            </Col>
-            <Col className="gutter-row" span={6}>
-              <div className="gutter-box">.ant-col-6</div>
-            </Col>
-            <Col className="gutter-row" span={6}>
-              <div className="gutter-box">.ant-col-6</div>
-            </Col>
+              {
+                intheaters_arr.map((item,index)=>{
+                  return(
+                    <Col key={item.id} className="gutter-row" span={6}>
+                    <Card  style={{ width: 240,height:444 }} bodyStyle={{ padding: 0 }}>
+                      <div className="custom-image">
+                        <img alt="example" width="100%" height="333px" src={item.images.medium} />
+                      </div>
+                      <div className="custom-card">
+                        <h3>{item.title}</h3>
+                        <p>{item.original_title}</p>
+                      </div>
+                    </Card>
+                    </Col>
+                  )
+                })
+              }
+
           </Row>
       </div>
+
     )
   }
 }
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Carousel } from 'antd';
+import { Carousel,Spin } from 'antd';
 import InTheaters from './InTheaters'
 import axios from 'axios'
 import './css/movie.css'
@@ -7,35 +7,65 @@ class Movie extends Component{
   constructor(props) {
     super(props);
     this.state={
-      intheaters_arr:null
+      initView:true,
+      intheaters_arr:null,
+      errorInfo:''
     }
   }
   componentDidMount(){
 
-    let url = "https://api.douban.com/v2/movie/in_theaters";
-    axios.get(url)
-      .then(response=>{
-        console.log(response.subjects);
+    // let url = "v2/movie/in_theaters";
+    // let url =`${\/api}v2/movie/in_theaters`
+    axios({
+      method:'get',
+      baseURL:'/api',
+      url:'v2/movie/in_theaters'
+      // url:'v2/book/1220562'
+    }).then(response=>{
+        console.log(response.data.subjects);
+        this.setState({
+          initView:false,
+          intheaters_arr:response.data.subjects
+        });
       })
       .catch(error=>{
         console.log(error);
+        this.setState({
+          initView:false,
+          errorInfo:error
+        });
       })
 
   }
   render(){
 
-    return(
-      <div className="carousel-div">
-        <Carousel autoplay effect="fade">
-          <div><h3>Movie Component1</h3></div>
-          <div><h3>Movie Component2</h3></div>
-          <div><h3>Movie Component3</h3></div>
-          <div><h3>Movie Component5</h3></div>
-        </Carousel>
-        <InTheaters/>
+    let {initView,intheaters_arr,errorInfo}=this.state;
+    if(initView){
+      return(
+        <div className="example">
+          <Spin />
+        </div>
+      )
+    }else if(intheaters_arr){
+      return(
+        <div className="carousel-div">
+          <Carousel autoplay effect="fade">
+            <div><h3>Movie Component1</h3></div>
+            <div><h3>Movie Component2</h3></div>
+            <div><h3>Movie Component3</h3></div>
+            <div><h3>Movie Component5</h3></div>
+          </Carousel>
+          <InTheaters intheaters_arr={intheaters_arr}/>
 
-      </div>
-    )
+        </div>
+        )
+    }else{
+      return(
+        <h1>信息异常{errorInfo}</h1>
+      )
+    }
+
+
   }
 }
 
