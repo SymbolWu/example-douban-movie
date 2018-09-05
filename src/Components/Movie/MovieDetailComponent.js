6e44f9b8bdd613b1b47c2b8a2a7b571800bca6d7
Wu Xinbo <bluessoul@sina.com>
2018-09-05 23:55:46 +0800

    clear all test note

--- src/Components/Movie/MovieDetailComponent.js
+++ src/Components/Movie/MovieDetailComponent.js
@@ -14,14 +14,11 @@ class MovieDetailComponent extends Component {
     this.store = props.store.movieHomeStore;
   }
   componentWillMount(){
-    //添加注释 online Test 2179
     let {movieItem,emptyLastmovieItem}=this.store;
-    //检测 store 中是否已经有movieItem ,如果有，且于当前所需要展示的电影ID不同，则提前清除。(6)
+    //检测 store 中是否已经有movieItem ,如果有，且于当前所需要展示的电影ID不同，则提前清除。
     (movieItem!==null&&this.props.match.params.movieid!==movieItem.id)?emptyLastmovieItem():console.log('last');
   }
   componentDidMount(){
-    //add 56
-    //new_
     let {fetchMoiveDetail}=this.store;
     fetchMoiveDetail(this.props.match.params.movieid);
   }
